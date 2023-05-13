92884ccdb4c0fba79a09ffd4770d1f3514b289e1
Демид <demid.kamaev@yandex.ru>
2023-05-13 11:48:48 +0300

    add Examle IF-Else

---
+++ Example005/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Даша")
+{
+    Console.WriteLine("УРа, это же Даша!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
